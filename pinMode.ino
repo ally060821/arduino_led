4939a89727118760afddf090c6681a42d8713176
Seoyeon An <52101527+ally060821@users.noreply.github.com>
2019-06-22 20:23:18 +0900

    Add files via upload

---
+++ pinMode.ino
@@ -0,0 +1,54 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+ 
+ pinMode(13,OUTPUT); 
+ digitalWrite(13,LOW);
+
+ //naming rule
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ // S //
+ digitalWrite(13,HIGH);
+ delay(200);
+ digitalWrite(13,LOW);
+ delay(200);
+ digitalWrite(13,HIGH);
+ delay(200);
+ digitalWrite(13,LOW);
+ delay(200);
+ digitalWrite(13,HIGH);
+ delay(200);
+ digitalWrite(13,LOW);
+ delay(500);
+ //O//
+  digitalWrite(13,HIGH);
+ delay(500);
+ digitalWrite(13,LOW);
+ delay(500);
+ digitalWrite(13,HIGH);
+ delay(500);
+ digitalWrite(13,LOW);
+ delay(500);
+ digitalWrite(13,HIGH);
+ delay(500);
+ digitalWrite(13,LOW);
+ delay(500);
+ //S//
+  digitalWrite(13,HIGH);
+ delay(200);
+ digitalWrite(13,LOW);
+ delay(200);
+ digitalWrite(13,HIGH);
+ delay(200);
+ digitalWrite(13,LOW);
+ delay(200);
+ digitalWrite(13,HIGH);
+ delay(200);
+ digitalWrite(13,LOW);
+ delay(500);
+}
